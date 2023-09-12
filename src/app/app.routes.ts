d9fee9689175529ab6dce6cb629d5a1c4b4a2e65
Joep Kockelkorn <joepkockelkorn@gmail.com>
2023-09-12 11:36:06 +0200

    chore: add sketch of routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // /                        HomeComponent (button to login)
+  // /login                   LoginComponent (redirect to overview or to the url that was requested before login)
+  // /books                   BooksComponent (list of data, filters, pagination, etc)
+  // /books/:bookId           DetailComponent (outlet for tabs, default tab is general)
+  // /books/:bookId/general   DetailGeneralComponent
+  // /books/:bookId/admin     DetailAdminComponent
+  // /404                     NotFoundComponent (in case of 404, replace the url)
+];
